c19de5d1731c7bf416a1e00a9b2abb24dcb56d8e
Anawil Karavek <akaravek@hotmail.com>
2024-01-16 14:31:13 +0100

    fix refactoring error

--- server/index.js
+++ server/index.js
@@ -3,23 +3,6 @@ const cors = require('cors')
 const routes = require('@util/routes')
 const errorMiddleware = require('@middleware/errorMiddleware')
 const loggerMiddleware = require('@middleware/loggerMiddleware')
-const personSchema = require('./models/person')
-
-mongoose.set('strictQuery', false)
-
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-
-mongoose.connect(url)
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB: ', error.message)
-  })
-
-const Person = mongoose.model('Person', personSchema) */
 
 const app = express()
 
